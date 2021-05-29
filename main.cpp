a09b4a7e0620029e57e35aa6f2e1d4dc920e5e68
Nishiki Asumi Yapa <>
2021-05-29 15:55:02 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+class Box{
+
+private:
+  double length;
+  double width;
+  double height;
+
+public:
+  Box();
+  Box(int l, int w, int h);
+  void setLength(int l);
+  void setWidth(int w);
+  void setHeight(int h);
+  double findVolume();
+
+};
+
+//(i)
+Box::Box(){
+  length = 0;
+  width = 0;
+  height = 0;
+}
+
+//(ii)
+Box::Box(int l, int w, int h){
+  length = l;
+  width = w;
+  height = h;
+}
+
+double Box::findVolume(){
+  return length * width * height;
+}
+
+using namespace std;
+
+int main() {
+  
+//(iii)
+  Box box1;
+
+//(iv)
+  Box box2(5,2,3);
+
+//(v)
+  int vol = box2.findVolume();
+  cout << "Volume is: " << vol;
+}
+
