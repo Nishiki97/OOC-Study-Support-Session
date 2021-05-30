182b1bd4dd1c75cdf46bb4bc0d250772da321ad1
Nishiki Asumi Yapa <>
2021-05-30 00:43:12 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,90 +1,40 @@
 #include <iostream>
 using namespace std;
-#define SIZE 20
 
-class Ward;
-class Team;
-class Doctor;
-class Patient;
-
-//Doctor class (parent)
-class Doctor{
-
-private:
-  Patient *patient[SIZE];
+//Picture class - parent class
+class Picture{
 
 protected:
-  string Speciality[10];
-  string location[10];
-  
-
-};
-
-//ConsultantDoctor class (child)
-class ConsultantDoctor:Doctor{
-
-private:
-  double fee;
-  Patient *patient[SIZE];
-  Team *team;
-};
-
-//JuniorDoctor class (child)
-class JuniorDoctor:Doctor{
-
-private:
-  int hours;
-
-};
-
-//Patient class (association-bi)
-class Patient{
-
-private:
-  string id;
-  string gender;
-  int age;
-  int Accepted;
-  string sickness[10];
-  string prescription[10];
-  string allergies[10];
-  string specialReq[10];
-  Doctor *doctor[SIZE];
-  ConsultantDoctor *cd;
-  Team *team;
-  Ward *w;
-};
-
-//Team class (association-bi/composition-whole)
-class Team{
+  string title;
+  double price;
 
 public:
-  string name;
-  ConsultantDoctor *cd;
-  Doctor *d;
-  Patient *p[SIZE];
+  void updatePrice();
 
 };
 
-//Ward class (association-bi/composition-part)
-class Ward{
+//Photograph class - child
+class Photograph{
 
 private:
-  string name;
-  string gender;
-  int capacity;
-  Patient *p[SIZE];
+  string photograph;
+  string camera;
+  int speed;
+  int aperture;
+
+public:
+  void alterContrast();
 
 };
 
-//Hospital class (composition-whole)
-class Hospital{
+//Painting class - child
+class Painting{
 
 private:
-  string name;
-  string address;
-  string phone;
-  Ward *w[SIZE];
-  Team *t[SIZE];
-  
+  string artist;
+  string type;
+  string owner;
+
+public:
+  void printprovenance();
 };
