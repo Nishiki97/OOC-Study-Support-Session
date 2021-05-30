e40b49d8ef52ed8263c0cc6184f16e133f256aa8
Nishiki Asumi Yapa <>
2021-05-30 00:36:17 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,36 +1,90 @@
 #include <iostream>
 using namespace std;
+#define SIZE 20
 
+class Ward;
+class Team;
+class Doctor;
+class Patient;
 
-//OrderDetails class (part)
-class OrderDetails{
+//Doctor class (parent)
+class Doctor{
 
 private:
-  int qty;
-  string taxStatus;
+  Patient *patient[SIZE];
 
-public:
-  void calcSubTotal();
-  float calcTax();
+protected:
+  string Speciality[10];
+  string location[10];
+  
 
 };
 
-//order class (whole)
-class Order{
+//ConsultantDoctor class (child)
+class ConsultantDoctor:Doctor{
 
 private:
-  int date;
-  string status;
-  double price;
-  OrderDetails *orderdetails; //define 'part' object here
-
-public:
-  //can add 'part' here using separate method call addOrderDetails()
-  void addOrderDetails(OrderDetails *od){
-    orderdetails = od;
-  }
-  void calcSubTotal();
-  float calcTax();
-  double calcTotal();
+  double fee;
+  Patient *patient[SIZE];
+  Team *team;
 };
 
+//JuniorDoctor class (child)
+class JuniorDoctor:Doctor{
+
+private:
+  int hours;
+
+};
+
+//Patient class (association-bi)
+class Patient{
+
+private:
+  string id;
+  string gender;
+  int age;
+  int Accepted;
+  string sickness[10];
+  string prescription[10];
+  string allergies[10];
+  string specialReq[10];
+  Doctor *doctor[SIZE];
+  ConsultantDoctor *cd;
+  Team *team;
+  Ward *w;
+};
+
+//Team class (association-bi/composition-whole)
+class Team{
+
+public:
+  string name;
+  ConsultantDoctor *cd;
+  Doctor *d;
+  Patient *p[SIZE];
+
+};
+
+//Ward class (association-bi/composition-part)
+class Ward{
+
+private:
+  string name;
+  string gender;
+  int capacity;
+  Patient *p[SIZE];
+
+};
+
+//Hospital class (composition-whole)
+class Hospital{
+
+private:
+  string name;
+  string address;
+  string phone;
+  Ward *w[SIZE];
+  Team *t[SIZE];
+  
+};
